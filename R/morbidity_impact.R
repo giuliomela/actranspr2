a50f16f47450939dd01fe2583542169be34076e2
giulio.mela <giulio.mela@gmail.com>
2025-01-27 11:51:11 +0100

    Fixed Reggio Calabria bug

--- R/morbidity_impact.R
+++ R/morbidity_impact.R
@@ -95,27 +95,27 @@ morbidity_impact <- function(city = "Roma",
 
   air_diseases <- length(unique(rr_air$outcome))
 
-  impact_phy <- do.call("rbind", replicate(phy_diseases, scenario, simplify = FALSE)) |>
-    dplyr::bind_cols(rr_phy) |>
+  impact_phy <- do.call("rbind", replicate(phy_diseases, scenario, simplify = FALSE)) %>%
+    dplyr::bind_cols(rr_phy) %>%
     dplyr::mutate(dimension = "phy")
 
-  impact_air <- do.call("rbind", replicate(air_diseases, scenario, simplify = FALSE)) |>
-    dplyr::bind_cols(rr_air) |>
+  impact_air <- do.call("rbind", replicate(air_diseases, scenario, simplify = FALSE)) %>%
+    dplyr::bind_cols(rr_air) %>%
     dplyr::mutate(dimension = "air")
 
   impact_l <- list(impact_air, impact_phy)
 
   impact <- dplyr::bind_rows(impact_l)
 
-  impact <- impact |>
-    dplyr::left_join(incidence) |>
+  impact <- impact %>%
+    dplyr::left_join(incidence) %>%
     dplyr::mutate(avoided_cases = .data$new_adopters * .data$incidence * !!as.name(paste0("rr_", mode_from, "_to_", mode_to)),
                   avoided_cases = ifelse(
                     .data$dimension == "phy",
                     .data$avoided_cases,
                     .data$avoided_cases * -1
                   ),
-                  incidence = NULL) |>
+                  incidence = NULL) %>%
     dplyr::select(!paste0("rr_", {{mode_from}}, "_to_", {{mode_to}}))
 
   impact
@@ -129,17 +129,17 @@ morbidity_impact <- function(city = "Roma",
     mode_to
   )
 
-  inj_rate <- actranspr2::road_inj_rate |>
-    dplyr::filter(city == {{city}} & mode %in% c({{mode_from}}, inj_mode)) |>
-    dplyr::select("age_class", "outcome", "mode", "road_inj_rate") |>
-    tidyr::pivot_wider(names_from = "mode", values_from = "road_inj_rate") |>
-    dplyr::group_by(.data$age_class) |>
+  inj_rate <- actranspr2::road_inj_rate %>%
+    dplyr::filter(city == {{city}} & mode %in% c({{mode_from}}, inj_mode)) %>%
+    dplyr::select("age_class", "outcome", "mode", "road_inj_rate") %>%
+    tidyr::pivot_wider(names_from = "mode", values_from = "road_inj_rate") %>%
+    dplyr::group_by(.data$age_class) %>%
     tidyr::nest()
 
 
-  inj_impact <- scenario |>
-    dplyr::left_join(actranspr2::age_classes) |>
-    dplyr::left_join(inj_rate) |>
+  inj_impact <- scenario %>%
+    dplyr::left_join(actranspr2::age_classes) %>%
+    dplyr::left_join(inj_rate) %>%
     tidyr::unnest(.data$data)
 
   inj_impact$diff_road_inj_rate <- inj_impact[[mode_from]] - inj_impact[[inj_mode]]
@@ -150,9 +150,9 @@ morbidity_impact <- function(city = "Roma",
 
   inj_impact[[inj_mode]] <- NULL
 
-  inj_impact <- inj_impact |>
+  inj_impact <- inj_impact %>%
     dplyr::mutate(avoided_cases = .data$km_year * .data$diff_road_inj_rate,
-                  dimension = "inj") |>
+                  dimension = "inj") %>%
     dplyr::select(!c("age_class", "diff_road_inj_rate"))
 
 impact <- dplyr::bind_rows(inj_impact, impact)
@@ -160,36 +160,31 @@ impact <- dplyr::bind_rows(inj_impact, impact)
 
   # adding life expectancy data
 
-  # nuts2_code <- actranspr2::codici_comuni_2023 |>
-  #   dplyr::filter(.data$`Denominazione in italiano` == {{city}}) |>
-  #   dplyr::pull(.data$`Codice NUTS2 2021 (3)`)
+  nuts2_code <- actranspr2::codici_comuni_2023 %>%
+    dplyr::filter(.data$`Denominazione in italiano` == {{city}}) %>%
+    dplyr::pull(.data$`Codice NUTS2 2021 (3)`)
 
   life_exp <- dplyr::select(actranspr2::demo_data,
-                            "city", "age", "life_exp") |>
-    dplyr::filter(.data$city == {{city}})
-  # |>
-  #   dplyr::group_by(.data$age) |>
-  #   dplyr::summarise(life_exp = mean(.data$life_exp)) |>
-  #   dplyr::ungroup()
-  #   # dplyr::ungroup() |>
-    # unique()
+                            "nuts2", "age", "life_exp") %>%
+    dplyr::filter(.data$nuts2 == nuts2_code) %>%
+    dplyr::ungroup() |>
+    unique()
 
-impact <- impact |>
+impact <- impact %>%
   dplyr::left_join(life_exp)
-
 
 # adding disease length data
 
 dis_length <- dplyr::select(actranspr2::disease_length,
                                 "disease", "cause_life_exp_decrease" ,
                                 "duration_years", "lifelong",
-                                "life_exp_decrease", "ages") |>
+                                "life_exp_decrease", "ages") %>%
   dplyr::mutate(outcome = .data$disease,
                 age = .data$ages,
                 .keep = "unused")
 
-impact <- impact |>
-  dplyr::left_join(dis_length) |>
+impact <- impact %>%
+  dplyr::left_join(dis_length) %>%
   dplyr::mutate(disease_length = dplyr::case_when(
     .data$lifelong == "yes" ~ .data$life_exp + .data$life_exp_decrease,
     .data$lifelong == "no" ~ .data$duration_years
@@ -201,8 +196,8 @@ dis_weights <- NULL
 
 for (i in c("acute", "chronic")) {
 
-  dis_weights[[i]] <- actranspr2::disability_weights |>
-    dplyr::filter(.data$phase == i) |>
+  dis_weights[[i]] <- actranspr2::disability_weights %>%
+    dplyr::filter(.data$phase == i) %>%
     dplyr::select("disease", "disability_weight")
 
   names(dis_weights[[i]]) <- c("outcome", i)
@@ -211,8 +206,8 @@ for (i in c("acute", "chronic")) {
 
 #adding disability weights
 
-impact <- impact |>
-  dplyr::left_join(dis_weights[["acute"]]) |>
+impact <- impact %>%
+  dplyr::left_join(dis_weights[["acute"]]) %>%
   dplyr::left_join(dis_weights[["chronic"]])
 
 #Life years gained/lost
@@ -228,6 +223,8 @@ impact$delta_life_yr <- purrr::map2_dbl(
 ) * impact$avoided_cases
 
 # Additional/avoided disability years
+
+
 
 impact$delta_dis_yr <- purrr::pmap_dbl(
   list(
@@ -245,28 +242,27 @@ impact$delta_dis_yr <- purrr::pmap_dbl(
 
 # monetisation
 
-impact <- impact |>
-  dplyr::group_by(.data$city, .data$outcome, .data$dimension) |>
+impact <- impact %>%
+  dplyr::group_by(.data$city, .data$outcome, .data$dimension) %>%
   dplyr::summarise(dplyr::across(c(.data$delta_life_yr, .data$delta_dis_yr),
-                                 function(x) sum(x * {{voly}}))) |>
-  dplyr::ungroup() |>
+                                 function(x) sum(x * {{voly}}))) %>%
+  dplyr::ungroup() %>%
   dplyr::mutate(dplyr::across(c(.data$delta_life_yr, .data$delta_dis_yr),
                               function(x) x / tot_km_year,
                               .names = "{.col}_km"),
                 mode_from = {{mode_from}},
-                mode_to = {{mode_to}}) |>
+                mode_to = {{mode_to}}) %>%
   dplyr::select("city", "mode_from", "mode_to", dplyr::everything())
-
 
 # traumatic deaths impact
 
-fatal_road_inj_rate <- actranspr2::fatal_road_inj_rate |>
-  dplyr::filter(city == {{city}} & mode %in% c({{mode_from}}, inj_mode)) |>
-  dplyr::select("age_class", "mode", "fatal_road_inj_rate") |>
+fatal_road_inj_rate <- actranspr2::fatal_road_inj_rate %>%
+  dplyr::filter(city == {{city}} & mode %in% c({{mode_from}}, inj_mode)) %>%
+  dplyr::select("age_class", "mode", "fatal_road_inj_rate") %>%
   tidyr::pivot_wider(names_from = .data$mode, values_from = .data$fatal_road_inj_rate)
 
-fatal_inj_impact <- scenario |>
-  dplyr::left_join(age_classes) |>
+fatal_inj_impact <- scenario %>%
+  dplyr::left_join(age_classes) %>%
   dplyr::left_join(fatal_road_inj_rate)
 
 fatal_inj_impact$diff_fatal_inj_rate <- fatal_inj_impact[[mode_from]] - fatal_inj_impact[[inj_mode]]
@@ -275,17 +271,17 @@ fatal_inj_impact[[mode_from]] <- NULL
 
 fatal_inj_impact[[inj_mode]] <- NULL
 
-fatal_inj_impact <- fatal_inj_impact |>
+fatal_inj_impact <- fatal_inj_impact %>%
   dplyr::mutate(avoided_deaths =  .data$km_year * .data$diff_fatal_inj_rate * {{vsl}}, # monetisation included
-                dimension = "inj") |>
+                dimension = "inj") %>%
   dplyr::select(!c("age_class", "diff_fatal_inj_rate"))
 
 delta_traumatic_deaths_km <- sum(fatal_inj_impact$avoided_deaths) / tot_km_year
 
 if (detail == "high"){
 
-  impact |>
-   # dplyr::select(!"delta_life_yr", "delta_dis_yr") |>
+  impact %>%
+   # dplyr::select(!"delta_life_yr", "delta_dis_yr") %>%
     dplyr::add_row(
       city = {{city}},
       mode_from = {{mode_from}},
@@ -294,22 +290,22 @@ if (detail == "high"){
       dimension = "inj",
       delta_life_yr_km = NA_real_,
       delta_dis_yr_km = NA_real_
-    ) |>
+    ) %>%
     dplyr::mutate(delta_traumatic_deaths_km = ifelse(
       .data$dimension == "inj" & .data$outcome == "traumatic death",
       delta_traumatic_deaths_km,
       NA_real_
-    )) |>
+    )) %>%
     dplyr::mutate(dplyr::across(dplyr::where(is.numeric), ~ ifelse(is.na(.x),
                                                                    0,
                                                                    .x)))
 
 } else if (detail == "medium"){
 
-  impact |>
-    dplyr::group_by(.data$dimension) |>
-    dplyr::summarise(dplyr::across(dplyr::contains("km"), ~ sum(., na.rm = T))) |>
-    dplyr::ungroup() |>
+  impact %>%
+    dplyr::group_by(.data$dimension) %>%
+    dplyr::summarise(dplyr::across(dplyr::contains("km"), ~ sum(., na.rm = T))) %>%
+    dplyr::ungroup() %>%
     dplyr::mutate(tram_deaths_km = ifelse(
       .data$dimension == "inj",
       delta_traumatic_deaths_km,
@@ -319,18 +315,18 @@ if (detail == "high"){
 
 } else if (detail == "low") {
 
-  impact <- impact |>
-    dplyr::group_by(.data$dimension) |>
-    dplyr::summarise(dplyr::across(dplyr::contains("km"), ~ sum(., na.rm = T))) |>
-    dplyr::ungroup() |>
+  impact <- impact %>%
+    dplyr::group_by(.data$dimension) %>%
+    dplyr::summarise(dplyr::across(dplyr::contains("km"), ~ sum(., na.rm = T))) %>%
+    dplyr::ungroup() %>%
     dplyr::mutate(tram_deaths_km = ifelse(
       .data$dimension == "inj",
       delta_traumatic_deaths_km,
       0
     ))
 
-  impact |>
-    dplyr::select(dplyr::contains("km")) |>
+  impact %>%
+    dplyr::select(dplyr::contains("km")) %>%
     sum(na.rm = TRUE)
 
 }
